2ecf432b1498907a815e79b223c41fefe994fccf
Steve Nail <snail71@gmail.com>
2016-04-20 15:11:38 +0000

    first commit

---
+++ autoBIAB.c
@@ -0,0 +1,17 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[])
+{
+	GtkBuilder *gtkBuilder;
+	GtkWidget *window;
+	gtk_init(&argc,&argv);
+	
+	gtkBuilder = gtk_builder_new();
+	gtk_builder_add_from_file(gtkBuilder,"autoBIAB.glade",NULL);
+	window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder,"winMain"));
+	g_object_unref(G_OBJECT(gtkBuilder));
+	gtk_widget_show(window);
+	gtk_main();
+	return 0;
+    
+}
---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -Wall -g -o autoBIAB autoBIAB.c `pkg-config --cflags --libs gtk+-3.0`
