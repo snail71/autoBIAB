97e1709be39b3ecf4294c094d93498db91684bfa
snail71 <snail71@gmail.com>
2016-05-02 13:42:00 -0500

    Updated PID to Arduino style and added auto pid tuning

--- pid.h
+++ pid.h
@@ -1,21 +1,76 @@
-#ifndef pid_H
-#define pid_H
+#ifndef PID_v1_h
+#define PID_v1_h
+#define LIBRARY_VERSION	1.0.0
 
-struct _pid {
-float pv; /*integer that contains the process value*/
-float sp; /*integer that contains the set point*/
-float integral;
-float pgain;
-float igain;
-float dgain;
-float deadband;
-float last_error;
-};
 
-void pid_init(struct _pid *warm, float process_point, float set_point);
-void pid_tune(struct _pid *pid, float p_gain, float i_gain, float d_gain, float dead_band);
-void pid_setinteg(struct _pid *pid,float new_integ);
-void pid_bumpless(struct _pid *pid);
-float pid_calc(struct _pid *pid);
+
+  //Constants used in some of the functions below
+  #define AUTOMATIC	1
+  #define MANUAL	0
+  #define DIRECT  0
+  #define REVERSE  1
+
+  //commonly used functions **************************************************************************
+    pid_Init(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
+        double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
+	
+    void pid_SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
+
+    bool pid_Compute();                       // * performs the PID calculation.  it should be
+                                          //   called every time loop() cycles. ON/OFF and
+                                          //   calculation frequency can be set using SetMode
+                                          //   SetSampleTime respectively
+
+    void pid_SetOutputLimits(double, double); //clamps the output to a specific range. 0-255 by default, but
+										  //it's likely the user will want to change this depending on
+										  //the application
+	
+
+
+  //available but not commonly used functions ********************************************************
+    void pid_SetTunings(double, double,       // * While most users will set the tunings once in the 
+                    double);         	  //   constructor, this function gives the user the option
+                                          //   of changing tunings during runtime for Adaptive control
+	void pid_SetControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
+										  //   means the output will increase when error is positive. REVERSE
+										  //   means the opposite.  it's very unlikely that this will be needed
+										  //   once it is set in the constructor.
+    void pid_SetSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
+                                          //   the PID calculation is performed.  default is 100
+										  
+										  
+										  
+  //Display functions ****************************************************************
+	double pid_GetKp();						  // These functions query the pid for interal values.
+	double pid_GetKi();						  //  they were created mainly for the pid front-end,
+	double pid_GetKd();						  // where it's important to know what is actually 
+	int pid_GetMode();						  //  inside the PID.
+	int pid_GetDirection();					  //
+
+ 
+	void pid_Initialize();
+	
+	double dispKp;				// * we'll hold on to the tuning parameters in user-entered 
+	double dispKi;				//   format for display purposes
+	double dispKd;				//
+    
+	double kp;                  // * (P)roportional Tuning Parameter
+    double ki;                  // * (I)ntegral Tuning Parameter
+    double kd;                  // * (D)erivative Tuning Parameter
+
+	int controllerDirection;
+
+    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
+    double *myOutput;             //   This creates a hard link between the variables and the 
+    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+                                  //   what these values are.  with pointers we'll just know.
+			  
+	unsigned long lastTime;
+	double ITerm, lastInput;
+
+	unsigned long SampleTime;
+	double outMin, outMax;
+	bool inAuto;
 
 #endif
+
---
+++ pid_at.h
@@ -0,0 +1,53 @@
+#ifndef PID_AutoTune_v0
+#define PID_AutoTune_v0
+#define LIBRARY_VERSION	0.0.1
+
+
+
+
+  //commonly used functions **************************************************************************
+    pidat_Init(double*, double*);                       	// * Constructor.  links the Autotune to a given PID
+    int pidat_Runtime();						   			   	// * Similar to the PID Compue function, returns non 0 when done
+	void pidat_Cancel();									   	// * Stops the AutoTune	
+	
+	void pidat_SetOutputStep(double);						   	// * how far above and below the starting value will the output step?	
+	double pidat_GetOutputStep();							   	// 
+	
+	void pidat_SetControlType(int); 						   	// * Determies if the tuning parameters returned will be PI (D=0)
+	int pidat_GetControlType();							   	//   or PID.  (0=PI, 1=PID)			
+	
+	void pidat_SetLookbackSec(int);							// * how far back are we looking to identify peaks
+	int pidat_GetLookbackSec();								//
+	
+	void pidat_SetNoiseBand(double);							// * the autotune will ignore signal chatter smaller than this value
+	double pidat_GetNoiseBand();								//   this should be acurately set
+	
+	double pidat_GetKp();										// * once autotune is complete, these functions contain the
+	double pidat_GetKi();										//   computed tuning parameters.  
+	double pidat_GetKd();										//
+	
+ 
+    void pidat_FinishUp();
+	bool isMax, isMin;
+	double *input, *output;
+	double setpoint;
+	double noiseBand;
+	int controlType;
+	bool running;
+	unsigned long peak1, peak2, lastTime;
+	int sampleTime;
+	int nLookBack;
+	int peakType;
+	double lastInputs[101];
+    double peaks[10];
+	int peakCount;
+	bool justchanged;
+	bool justevaled;
+	double absMax, absMin;
+	double oStep;
+	double outputStart;
+	double Ku, Pu;
+	
+
+#endif
+
