fc517adff80eb079abdad99e44faa07a8b893f05
Steve Nail <snail71@gmail.com>
2016-05-23 12:58:07 -0500

    added equipment file

---
+++ equipment.h
@@ -0,0 +1,12 @@
+#ifndef equipment_h
+#define equipment_h
+
+struct equipment
+{
+	float 			kettleSizeGallons;
+	float 			grainAbsorption;
+	float			evaporationPerGallon;
+	float 			trubLoss;	
+};
+
+#endif
